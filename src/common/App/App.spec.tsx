8154ced2c07aa02b539230c57c69983d9bb02d67
Michal Zalecki <michal@michalzalecki.com>
2018-08-08 15:54:45 +0200

    Use TypeScript

--- src/common/App/App.spec.tsx
+++ src/common/App/App.spec.tsx
@@ -1,7 +1,6 @@
-import React from "react";
 import { shallow } from "enzyme";
+import React from "react";
 import { App } from "./App";
-
 
 describe("App", () => {
   it("adds bang after making a bang", () => {
